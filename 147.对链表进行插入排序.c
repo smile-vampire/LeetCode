43f31e78a722beb958620ef3fc3f03b367226ec8
smile-vampire <yankai6833@qq.com>
2019-12-02 20:11:25 +0800

    对链表进行插入排序

--- 147.对链表进行插入排序.c
+++ 147.对链表进行插入排序.c
@@ -12,7 +12,9 @@
  *     struct ListNode *next;
  * };
  */
-
+//插入链表元素 先构造一个新的带头空链表  让第一个放到新链表下一个
+//然后从链表上取元素与之比较如果大的放到后面  小的插到前面
+//新构造的链表要记录前一个值和每次更新的链表尾部
 typedef struct ListNode Node;
 struct ListNode* insertionSortList(struct ListNode* head){
     if(head==NULL || head->next==NULL)
