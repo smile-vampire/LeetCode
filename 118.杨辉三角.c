9d2f7c7b18615f5a66779356cb596b2c593cc4b6
smile-vampire <yankai6833@qq.com>
2019-12-16 22:04:03 +0800

    杨辉三角

--- 118.杨辉三角.c
+++ 118.杨辉三角.c
@@ -1,17 +1,18 @@
-int** generate(int numRows, int* returnSize, int** returnColumnSizes){
-    *returnSize = numRows;
-    *returnColumnSizes = (int*)malloc(sizeof(int) * numRows);//returnColumnSizes储存杨辉三角每一行元素的个数
-    int **nums;
-    nums = (int**)malloc(sizeof(int*) * (*returnSize));
-    for (int i = 0; i < numRows; i++){
-        (*returnColumnSizes)[i] = i + 1;
-        nums[i] = (int*)malloc(sizeof(int) * (*returnColumnSizes)[i]);
-        for (int j = 0; j <= i; j++){
-            if (j == 0 || j == i)
-                nums[i][j] = 1;
-            else
-                nums[i][j] = nums[i - 1][j - 1] + nums[i - 1][j];
+
+int** generate(int numRows,int** ColumnSizes){
+    int i,j;
+    int** triangle = malloc(numRows*sizeof(int*));
+    *ColumnSizes=malloc(numRows*sizeof(int*));
+    for(i=0;i<numRows;i++)
+    {
+        int num=i+1;
+        (*ColumnSizes)=num;
+        triangle[i][0]=1;
+        triangle[i][num-1]=1;
+        for(j=1;j<=num;j++)
+        {
+            triangle[i][j]=triangle[i-1][j-1]+triangle[i-1][j];
         }
     }
-    return nums;
+    return triangle;
 }
